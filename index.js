9a9e4718c72d61c9ddcdfee1a0df1c3462c07b5a
unknown <iamsobahan@gmail.com>
2021-10-31 22:22:01 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,100 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v2wiu.mongodb.net/myFirstDatabase?retryWrites=true&w=majorit`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// 8XM85OmSF37MD5f3
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("db_packages");
+    const userPackages = database.collection("packages");
+
+    const orderDb = client.db("order_packages");
+    const orderInfo = orderDb.collection("infos");
+
+    app.get("/packages", async (req, res) => {
+      const cursor = userPackages.find({});
+      const packages = await cursor.toArray();
+      res.send(packages);
+    });
+
+    app.post("/packages", async (req, res) => {
+      const packages = req.body;
+      const result = await orderInfo.insertOne(packages);
+      res.send(result);
+    });
+
+    app.get("/packages/orders", async (req, res) => {
+      const cursor = orderInfo.find({});
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.put("/packages/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const update = req.body;
+      const filter = { _id: ObjectId(id) };
+      const option = { upsert: true };
+      const updateDoc = {
+        $set: {
+          status: update.status,
+        },
+      };
+      const result = await orderInfo.updateOne(filter, updateDoc, option);
+      console.log(result);
+      res.json(result);
+    });
+
+    app.get("/packages/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await userPackages.findOne(query);
+      res.send(result);
+    });
+
+    app.delete("/packages/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderInfo.deleteOne(query);
+      res.json(result);
+    });
+
+    app.delete("/packages/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderInfo.deleteOne(query);
+      res.json(result);
+    });
+
+    app.post("/packages", async (req, res) => {
+      const orders = req.body;
+      const result = await orderInfo.insertOne(orders);
+      console.log(result);
+      res.json(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  console.log("hit the server");
+  res.send("Allah is our Creator");
+});
+
+app.listen(port, () => {
+  console.log("listening from ", port);
+});
